18cdb5a80166a5b76ea1187a12cafb5cd033226c
chetna <guptachetna96@gmail.com>
2023-06-19 15:20:38 +0530

    python datatypes

---
+++ indexing.py
@@ -0,0 +1,5 @@
+str1='hello world'
+print(len(str1))
+print(str1[0])
+print(str1[-1])
+print(str1[-5])
---
+++ string_immutability.py
@@ -0,0 +1,6 @@
+str1='hello'
+print(str1)
+print(id(str1))
+str1='world'
+print(str1)
+print(id(str1))
