32b20c3f8ea018b97dfb9c8ad70aaddacf09bf11
chetna <guptachetna96@gmail.com>
2023-06-20 12:16:37 +0530

    assignment practice

---
+++ assignment.py
@@ -0,0 +1,52 @@
+# assign
+num1 =5
+print(num1)
+
+# Add and assign
+num1+=5
+print(num1)
+
+# Subtract and assign
+num1-=2
+print(num1)
+
+# Multiply and assign
+num1*=2
+print(num1)
+
+# Divide and assign
+num1/=2
+print(num1)
+
+# Floor and assign
+num1//=2
+print(num1)
+
+# Exponent and assign
+num1**=3
+print(num1)
+
+# Modulo and assign
+num1%=2
+print(num1)
+
+# Or and assign
+num1=5
+num1|=2
+print(num1)
+
+# And and assign
+num1&=2
+print(num1)
+
+# Xor and assign
+num1^=2
+print(num1)
+
+# Right shift and assign
+num1>>=2
+print(num1)
+
+# Left shift and assign
+num1<<=2
+print(num1)
