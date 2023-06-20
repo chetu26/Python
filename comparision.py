2c0744d1db658ee3155a6653466b982707fa815a
chetna <guptachetna96@gmail.com>
2023-06-20 11:51:23 +0530

    comparision operator

---
+++ comparision.py
@@ -0,0 +1,26 @@
+num1=10.5
+num2=10
+
+# greater than
+print(num1>num2)
+
+# less than
+print(num1<num2)
+
+# greater than and equal To
+print(num1>=num2)
+
+# less than and equal 
+print(num1<=num2)
+
+# equal 
+print(num1==num2)
+
+# not equal
+print(num1!=num2)
+
+# equal(identity)
+print(num1 is num2)
+
+# not equal (identity)
+print(num1 is not num2)
