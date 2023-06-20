4d6690228250a6c8ef24e8b81e73c57546236f6c
chetna <guptachetna96@gmail.com>
2023-06-20 12:35:56 +0530

    bitwise operators

---
+++ bitwise.py
@@ -0,0 +1,21 @@
+num1=5
+num2=2
+
+# Bitwise and
+print(num1 & num2)
+
+# Bitwise or
+print(num1 | num2)
+
+# Bitwise xor
+print(num1 ^ num2)
+
+# Bitwise not
+print(~num1)
+
+# Bitwise left shift
+print(num1 << num2)
+
+# Bitwise right shift
+print(num1 >> num2)
+
