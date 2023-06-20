39e0be371f59554f21f6ec54e6e7d300f7005411
chetna <guptachetna96@gmail.com>
2023-06-20 11:36:48 +0530

    arithematic practice

---
+++ arithematic.py
@@ -0,0 +1,33 @@
+# arithematic operations
+
+# addition
+print(5.5+4.5)
+print(10 + 9.6)
+
+# subtraction
+print(-5.5+4.5)
+print(10 - 9.6)
+
+# multipication
+print(10.0*2.1)
+print(10*2.1)
+
+# division
+print(10.0/2.1)
+print(10/2.1)
+
+# modulo
+print(10.0%2.1)
+print(10%2.1)
+
+# floor
+print(10.0//2.1)
+print(10//2.1)
+
+# exponent
+print(10.0**2.1)
+print(10**2.1)
+
+# parenthesis
+print(5 * (4.3+9))
+
